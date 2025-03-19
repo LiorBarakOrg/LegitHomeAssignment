dce94ed770f40585e644c52aa0dc4ff2b9d529e1
liorbarak94 <liorbarak94@gmail.com>
2025-03-19 12:23:18 +0200

    make sure it runs

--- obj/Debug/net8.0/LegitHomeAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/LegitHomeAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegitHomeAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f4d89cc373532115d4136e4f780cb27b1d3bdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegitHomeAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegitHomeAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
